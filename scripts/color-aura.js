122d83dd258154203de4b5a27140a3e34d4a3502
Lxtharia <87075045+Lxtharia@users.noreply.github.com>
2023-03-17 23:14:07 +0100

    Make the aura change color on click, add onlick-animated cursor decoration

--- scripts/color-aura.js
+++ scripts/color-aura.js
@@ -1,15 +1,40 @@
 let auraElement = document.getElementById("color-aura");
-let auraRadius = 100;
+let cursorTrackingDivs = document.getElementsByClassName("cursor-tracking");
+let cursorCrosshair = document.getElementById("cursor-crosshair")
+let rotateOffset = 0;
 
-auraElement.style.width = auraElement.style.height = auraElement.style.borderRadius = auraRadius + "px";
+//styling in javascript, just because i might need it
+let crossHairSize = 70;
+cursorCrosshair.style.width = cursorCrosshair.style.height = crossHairSize + "px";
 
 let mouseX, mouseY;
 
-function adjustAuraPosition() {
-    auraElement.style.left = window.scrollX + mouseX - auraRadius / 2 + "px";
-    auraElement.style.top  = window.scrollY + mouseY - auraRadius / 2 + "px";
-    // console.log("MOVED! " + e.clientX);
+function updateTrackingCursorPosition() {
+    for (let CTdiv of cursorTrackingDivs){
+        CTdiv.style.left = window.scrollX + mouseX + "px";
+        CTdiv.style.top = window.scrollY + mouseY  + "px";
+    }
+}
+function stylesOnMouseDown() {
+    rotateOffset -= 45;
+    // cursorCrosshair.style.width = crossHairSize*4/5 + "px";
+    // cursorCrosshair.style.height = crossHairSize*4/5 + "px";
+    cursorCrosshair.style.rotate = rotateOffset-10+"deg";
+    auraElement.className += " onmousedown-color"
 }
 
-addEventListener("mousemove", (e) => {mouseX = e.clientX; mouseY = e.clientY; adjustAuraPosition()});
-addEventListener("scroll", adjustAuraPosition);
+function stylesOnMouseUp() {
+    // cursorCrosshair.style.width = crossHairSize + "px";
+    // cursorCrosshair.style.height = crossHairSize + "px";
+    cursorCrosshair.style.rotate = rotateOffset + "deg";
+    auraElement.className = auraElement.className.replace(/\b\s*onmousedown-color\b/, '');
+}
+
+addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    updateTrackingCursorPosition()
+});
+addEventListener("scroll", updateTrackingCursorPosition);
+addEventListener("mousedown", stylesOnMouseDown);
+addEventListener("mouseup", stylesOnMouseUp);
